d5502dc08ae2d5574314a204deebc1632de6c0a4
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-01 23:13:32 +0600

    solved the issue in import connectDb. Added file extension

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from "express"
 import colors from "colors"
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDb from "./config/db.js";
+
 
 // configure env
 dotenv.config();
 
+// database connect
+connectDb();
 
 
 // rest object
 const app = express();
+
+// middle wares
+app.use(express.json);
+app.use(morgan('dev'));
 
 // rest api
 app.get('/', (req, res) => {
