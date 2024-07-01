62a18795169ff8d3db3c5062852989d2b461648f
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-02 03:24:53 +0600

    added response of each error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,29 @@
-export const registerController = () => {}
+import usermodel from "../models/userModel.js"
+
+export const registerController = async(req, res) => {
+    try{
+        const {name, email, password, phone, address} = req.body;
+        if(!name){
+            return res.send({error: "Name is required"});
+        }
+        if(!email){
+            return res.send({error: "Email is required"});
+        }
+        if(!password){
+            return res.send({error: "Passsword is required"});
+        }
+        if(!phone){
+            return res.send({error: "Phone is required"});
+        }
+        if(!address){
+            return res.send({error: "Address is required"});
+        }
+    } catch(error){
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "error in registration",
+            error
+        })
+    }
+}
