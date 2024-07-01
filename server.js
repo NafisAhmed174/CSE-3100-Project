20ebd22b253291cdfe1ef4d1d8544bcb1e66267d
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-02 00:59:09 +0600

    added authroutes in main server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors"
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDb from "./config/db.js";
+import authRoutes from "./routes/authRoute.js"
 
 
 // configure env
@@ -18,6 +19,9 @@ const app = express();
 // middle wares
 app.use(express.json);
 app.use(morgan('dev'));
+
+// routes
+app.use('/api/v1/auth', authRoutes);
 
 // rest api
 app.get('/', (req, res) => {
