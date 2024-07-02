951d1f664d4bf91506529798d564d64b6abf34db
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-02 16:49:10 +0600

    created login controller

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import {registerController} from "../controllers/authController.js"
+import {registerController, loginController} from "../controllers/authController.js"
 
 // router object
 const router = express.Router();
 
 // routing
 // REGISTER || METHOD POST
-
 router.post('/register', registerController);
+
+// LOGIN || METHOD POST
+router.post('/login', loginController);
 
 export default router
