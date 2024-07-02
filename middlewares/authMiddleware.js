7588e90711e3a94c993ab80d1ebfd24531df342a
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-02 19:21:26 +0600

    tested the middlewares

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,6 +6,7 @@ import userModel from '../models/userModel.js';
 export const requireSignIn = async (req, res, next) => {
     try{
         const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET);
+        req.user = decode;
         next();
     } catch(error){
         console.log(error);
