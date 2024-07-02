876d2f04662379f70a0b5c15d9f6f160d0058bc9
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-02 20:28:21 +0600

    created header, footer, layout in layout

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h1>Footer comp</h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div>
+        <h1>Layout</h1>
+    </div>
+  )
+}
+
+export default layout
