359708ffd183a6ce6a24ad4c0663a4075cd5d40a
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-03 18:49:02 +0600

    created routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import Layout from './components/Layout/Layout.js';
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
-      <Layout> 
+    <Routes>
+      <Route path='/' element = {<HomePage/>}/>
+      <Route path='/about' element = {<About/>}/>
+      <Route path='/contact' element = {<Contact/>}/>
+      <Route path='/policy' element = {<Policy/>}/>
+      <Route path='*' element = {<PageNotFound/>}/>
+
+    </Routes>
+      
         <h1>Ecommerce App</h1>
-      </Layout>
     </>
   );
 }
