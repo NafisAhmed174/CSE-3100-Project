5f567ff90826531fd20f96c0e4c35247a5dadff3
NafisAhmed174 <nafisahmed947@gmail.com>
2024-07-10 04:08:34 +0600

    created register page and its route

---
+++ client/src/pages/auth/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const Register = () => {
+  return (
+    <Layout title={'Register MobileShop'}>
+        <h1>Register page</h1>
+    </Layout>
+  )
+}
+
+export default Register;
