bd9925945cf404fd586fc4a5aa76b31f9702e4dd
NafisAhmed174 <nafisahmed947@gmail.com>
2024-12-31 01:28:26 +0600

    added react-hot-toast

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Header from './Header';
 import Footer from './Footer';
 import {Helmet} from "react-helmet";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({children, title = 'Mobile Shop', description = 'Mern Stack Project', keywords = 'mern, react, node, mongoDb', author = 'Nafis Ahmed'}) => {
   return (
@@ -16,7 +15,7 @@ const Layout = ({children, title = 'Mobile Shop', description = 'Mern Stack Proj
         </Helmet>
         <Header/>
         <main className='content'>
-            <ToastContainer/>
+            <Toaster/>
             {children}
         </main>
         <Footer/>
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import Layout from "../../components/Layout/Layout";
-import { toast } from "react-toastify";
+import toast from "react-hot-toast";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom"
 
