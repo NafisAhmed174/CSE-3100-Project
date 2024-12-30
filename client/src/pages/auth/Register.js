a2ad243f542fc37265c1a41c6c0617d37ef7f299
NafisAhmed174 <nafisahmed947@gmail.com>
2024-12-30 20:52:27 +0600

    added basic register form

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,12 +1,62 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React from "react";
+import Layout from "../../components/Layout/Layout";
 
 const Register = () => {
   return (
-    <Layout title={'Register MobileShop'}>
-        <h1>Register page</h1>
+    <Layout title={"Register MobileShop"}>
+      <div className="register">
+        <h1> Register page</h1>
+        <form>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your name"
+            />
+          </div>
+
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your mail"
+            />
+          </div>
+
+
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your phone no"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your adress"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default Register;
