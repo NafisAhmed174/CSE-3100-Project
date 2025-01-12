b6959d3d1d37192995f4c70c406089876da2a2db
NafisAhmed174 <nafisahmed947@gmail.com>
2025-01-12 10:47:43 +0600

    started 12 jun

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom"
 import "../../styles/AuthStyles.css";
 
-const Register = () => {
+const Login = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,25 +15,30 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      const res = await axios.post("/api/v1/auth/register",{name, email, password, phone, address});
-      if(res.data.success){
+    try {
+      const res = await axios.post("/api/v1/auth/register", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+      if (res.data.success) {
         toast.success(res.data.message);
-        navigate('/login')
-      }else{
+        navigate("/login");
+      } else {
         toast.error(res.data.message);
       }
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
       toast.error("Something went wrong");
     }
-  }
+  };
 
   return (
     <Layout title={"Register MobileShop"}>
-      <div className="form-container" >
-        <h4 className="title"> Register page</h4>
+      <div className="form-container">
+        <h4 className="title"> Login   page</h4>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <input
@@ -58,7 +63,6 @@ const Register = () => {
               required
             />
           </div>
-
 
           <div className="mb-3">
             <input
@@ -102,4 +106,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
